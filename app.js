4b2e243e27a1c81ac9adabe472f262732695975a
Paras <parasthind299@gmail.com>
2022-01-08 22:55:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+
+const {connectDB} = require('./config/db')
+const userRouter = require('./routes/auth')
+
+const app = express()
+
+connectDB()
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// ROUTES
+app.use('/', userRouter)
+
+const port = process.env.port || 8000
+app.listen(port, (res) => {
+  console.log(`Server running on PORT ${port}`)
+})
