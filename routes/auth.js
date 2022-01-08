d7b110680cafc59b7708b8d5b579d6bbe9d2d5e7
Paras <parasthind299@gmail.com>
2022-01-08 22:56:03 +0530

    login signup done

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+// const {login, register} = require('../controllers/authController')
+
+router.post('/register')
+router.post('/login')
+
+module.exports = router
